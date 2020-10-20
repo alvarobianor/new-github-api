d0927a74586e07137e53540037cc68bd45e85fd1
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-10-20 17:14:44 -0300

    Styling Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Title, Form, Repositories} from './style'
+import Logo from '../../assets/logo.svg'
 
 const Dashboard: React.FC = () => {
   return (
     <>
-    <h1>Explore repositories on Github</h1>
-    
+    <img src={Logo} alt="logo of aplication"/>
+    <Title>Explore repositories on Github</Title>
+    <Form>
+      <input type="text" placeholder="Text here"/>
+      <button type="submit">Search</button>
+    </Form>
+    <Repositories>
+    <a href="test">
+      <img src="https://avatars3.githubusercontent.com/u/19610639?s=460&u=beeb3184574cb7862579a00ccfe2dfc935b6f00e&v=4" alt="Álvaro Bianor" />
+      <div>
+        <strong>Alvim rei delas</strong>
+        <p>Caba bom desenrolado</p>
+      </div>
+    </a>
+            
+    </Repositories>
     </>
 
   );
