ea51478ac1512e8bbb48dc55ddc338043cf828a6
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-10-20 23:53:43 -0300

    Deleting some comments

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -66,21 +66,3 @@ const Repository: React.FC = () => {
 };
 
 export default Repository;
-
-
-// import React from 'react';
-// import {useRouteMatch} from 'react-router-dom'
-
-// interface Params {
-//   repository: string;
-// }
-
-// const Repository: React.FC = () => {
-//   const {params} = useRouteMatch<Params>()
-
-//   return (
-//   <h1>Olá repo: {params.repository}</h1>
-//   );
-// }
-
-// export default Repository;
